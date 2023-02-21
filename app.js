e6fba16c0f1d1633170f1e3664090587a68029db
abdouwarissiboukari <wariss7@gmail.com>
2023-02-21 17:33:46 +0000

    Adds of 2023-02-21

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express')
+let pokemons = require('./mock-pokemon')
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello, Express!'))
+app.get('/api/pokemon/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const pokemon = pokemons.find(pokemon => pokemon.id===id)
+  res.send(`Vous aveez demandé le pokemon ${pokemon.name}`)
+})
 
-app.listen(port, () => console.log('Notre application Node est démqrrée sur : http://localhost:' + port))
+app.get('/api/pokemons', (req, res) =>
+  {
+    const pokemonsCount = pokemons.length
+    res.send(`Il y a ${pokemonsCount} pokémon dans le pokedex pour le moment`)
+  }
+)
+
+app.listen(port, () => console.log(`Notre application Node est démqrrée sur : http://localhost:${port}`))
