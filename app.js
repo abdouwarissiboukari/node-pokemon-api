7e9c8c70ce0cbc3661cfe83efa6acf23f940e945
Abdou Warissi Boukari <wariss7@gmail.com>
2023-02-24 11:27:15 +0000

    Commit n°1 from baw#mba-waris

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const helper = require('./helper.js')
+const {success} = require('./helper.js')
 let pokemons = require('./mock-pokemon')
 
 const app = express()
@@ -8,13 +10,17 @@ app.get('/', (req, res) => res.send('Hello, Express!'))
 app.get('/api/pokemon/:id', (req, res) => {
   const id = parseInt(req.params.id)
   const pokemon = pokemons.find(pokemon => pokemon.id===id)
-  res.send(`Vous aveez demandé le pokemon ${pokemon.name}`)
+  const message = "Un pokémon a été bien retrouvé."
+  // res.send(`Vous aveez demandé le pokemon ${pokemon.name}`)
+  // res.json(helper.success(message, pokemon))
+  res.json(success(message, pokemon))
 })
 
 app.get('/api/pokemons', (req, res) =>
   {
-    const pokemonsCount = pokemons.length
-    res.send(`Il y a ${pokemonsCount} pokémon dans le pokedex pour le moment`)
+    // const pokemonsCount = pokemons.length
+    // res.send(`Il y a ${pokemonsCount} pokémon dans le pokedex pour le moment`)
+    res.json(success('La liste des pokémons a bien été récupée', pokemons))
   }
 )
 
---
+++ helper.js
@@ -0,0 +1,25 @@
+exports.success = (message, data) => {
+  return {message, data }
+}
+
+
+
+
+// exports.success = (message, data) => {
+//   return {
+//     message: message,
+//     data: data
+//   }
+// }
+
+// exports.success
+
+// const success = (message, data) => {
+//   return {
+//     message: message,
+//     data: data
+//   }
+// }
+
+// exports.success
+
