b163505eecede9cf26fb11adfcc11572eb10ee6d
abdouwarissiboukari <wariss7@gmail.com>
2023-03-29 17:17:30 +0000

    Adds of 2023-03-28 API REST fin de la Gestion des erreurs

--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
       where: { id: id }
     })
     .then(_ => {
-      Pokemon.findByPk(id)
+      return Pokemon.findByPk(id)
       .then(pokemon => {
         if(pokemon === null){
           const message = `Le pokémon demandé n'existe pas. Réessayez avec un autre identifiant.`
@@ -16,13 +16,9 @@ module.exports = (app) => {
         const message = `Le pokémon ${pokemon.name} a bien été modifié.`
         res.json({message, data: pokemon })
       })
-      .catch(error => {
-        const message = `Le n'apas pu être modifié. Réessayer dans quelques instants.`
-        res.status(500).json({message, data: error})
-      })
     })
     .catch(error => {
-      const message = `Le n'apas pu être modifié. Réessayer dans quelques instants.`
+      const message = `Le pokémon n'a pas pu être modifié. Réessayer dans quelques instants.`
       res.status(500).json({message, data: error})
     })
   })
