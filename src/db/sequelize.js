87ea60c1cd860b1710760a2d6093e1b48b0b4598
Abdou Warissi Boukari <wariss7@gmail.com>
2023-06-10 18:56:42 +0000

    Commit n°7 from baw#mba-waris Deploiement #2 Heroku database configuration set

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -3,8 +3,42 @@ const PokemonModel = require('../models/pokemon')
 const UserModel = require('../models/user')
 const pokemons = require('./mock-pokemon')
 const bcrypt = require('bcrypt')
-  
-const sequelize = new Sequelize(
+
+let sequelize
+
+if(process.env.NODE_ENV === 'production'){
+  sequelize = new Sequelize(
+    'bmp41i3e2p3zmx9t',
+    'nhna5ko5j0ouqnnx',
+    'pokedex46',
+    {
+      host: 'q0h7yf5pynynaq54.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      dialect: 'mariadb',
+      dialectOptions: {
+        timezone: '+00:00'
+      },
+      logging: true,
+      timezone: '+00:00'
+    }
+  )
+}else{
+  sequelize = new Sequelize(
+    'db_a64746_pokedex',
+    'a64746_pokedex',
+    'pokedex46',
+    {
+      host: 'mysql5025.site4now.net',
+      dialect: 'mysql',
+      dialectOptions: {
+        timezone: '+00:00'
+      },
+      logging: false,
+      timezone: '+00:00'
+    }
+  )
+}
+
+sequelize = new Sequelize(
   'db_a64746_pokedex',
   'a64746_pokedex',
   'pokedex46',
@@ -23,7 +57,7 @@ const Pokemon = PokemonModel(sequelize, DataTypes)
 const User = UserModel(sequelize, DataTypes)
   
 const initDb = () => {
-  return sequelize.sync({force: true}).then(_ => {
+  return sequelize.sync().then(_ => {
     pokemons.map(pokemon => {
       Pokemon.create({
         name: pokemon.name,
